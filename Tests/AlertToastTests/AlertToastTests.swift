00c57279b1b7305b8deede16f9799c3326a04cc5
elai950 <elai950@gmail.com>
2021-02-10 11:02:39 +0200

    Bug fix

--- Tests/AlertToastTests/AlertToastTests.swift
+++ Tests/AlertToastTests/AlertToastTests.swift
@@ -6,7 +6,7 @@ final class AlertToastTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AlertToast(type: .none, title: "Title").title, "Hello, World!")
+        XCTAssertEqual(AlertToast(type: .regular, title: "Title").title, "Hello, World!")
     }
 
     static var allTests = [
