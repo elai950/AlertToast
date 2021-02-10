c7f64062e68e41b72ee60b94fa41c3650e48c226
elai950 <elai950@gmail.com>
2021-02-10 20:44:48 +0200

    Added localization support

--- Sources/AlertToast/AlertToast.swift
+++ Sources/AlertToast/AlertToast.swift
@@ -127,10 +127,10 @@ public struct AlertToast: View{
             }
             
             VStack(spacing: type == .regular ? 8 : 2){
-                Text(title)
+                Text(LocalizedStringKey(title))
                     .bold()
                 if subTitle != nil{
-                    Text(subTitle!)
+                    Text(LocalizedStringKey(subTitle ?? ""))
                         .font(.footnote)
                         .opacity(0.7)
                 }
