b821a7a571c505b6c49b3cc0b1882a16ee8f2b70
Benjamin Barnard <44757473+barnard-b@users.noreply.github.com>
2021-02-12 21:06:33 -0500

    Added macOS support.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "AlertToast",
-    platforms: [.iOS(.v13)],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
