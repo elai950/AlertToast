3af44ac2db9f978128d821fab82fad3b8b57450a
zunda <zunda.dev@gmail.com>
2023-06-04 22:45:45 +0900

    add swift-format and format

--- Package.swift
+++ Package.swift
@@ -4,29 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "AlertToast",
-    platforms: [
-        .iOS(.v14),
-        .macOS(.v11)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AlertToast",
-            targets: ["AlertToast"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AlertToast",
-            dependencies: []),
-        .testTarget(
-            name: "AlertToastTests",
-            dependencies: ["AlertToast"]),
-    ]
+  name: "AlertToast",
+  platforms: [
+    .iOS(.v14),
+    .macOS(.v11)
+  ],
+  products: [
+    .library(
+      name: "AlertToast",
+      targets: ["AlertToast"]
+    ),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-format", .upToNextMajor(from: "508.0.1")),
+  ],
+  targets: [
+    .target(
+      name: "AlertToast",
+      dependencies: []
+    ),
+    .testTarget(
+      name: "AlertToastTests",
+      dependencies: ["AlertToast"]
+    ),
+  ]
 )
